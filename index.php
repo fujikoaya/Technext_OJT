4c975f1a6e0000a3d59dde031c7d4da8473f36d0
apple <wonderfulaya.2020@gmail.com>
2022-11-08 22:55:41 -0800

    htmlとcss追加

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="style.css">
+    <title>OJT</title>
+</head>
+<body>
+    <header>
+        <div class='header'>
+           <div class='header-left'>
+                <img src="./img/logo.png" alt="">
+           </div>
+           <div class='header-right'>
+                <ul>
+                <li><a href="#">Menu01</a></li>
+                <li><a href="#">Meun02</a></li>
+                <li><a href="#">Meun03</a></li>
+                <li><a href="#">Meun04</a></li>
+                </ul>
+           </div>
+        </div>        
+    </header>
+    <main>
+      <div class='main-titile'>
+        <div class='container'>
+          <div class='mainvisual'>
+            <div class='kv'>
+              <picture>
+               <source srcset="./img/visual_1_pc.png" media='(min-width: 768px)'>
+               <!-- mediaの中にレスポシブサイズを記入 -->
+               <img src="./img/visual_1_sp.png" alt="">
+              </picture>
+              <div class='kv-wrap'>
+              <picture>
+               <source srcset="./img/visual_text_pc.png" media='(min-width: 768px'>
+               <!-- mediaの中にレスポシブサイズを記入 -->
+                <img src="./img/visual_text_sp.png" alt="">
+              </picture>
+            </div>
+          </div>   
+            <p class='text'>進化し続ける「街」
+                <br>
+                アメリカンビレッジマガジン</p>
+          </div> 
+          <section class='latest article'>
+           <h1>Latest Article</h1>
+            <div class='la'>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_1.png" alt="">
+                    <p>2018 / 5 / 20</p>
+                    <p>おしゃれなカフェありますよ</p>
+                    <div class='readmore'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_2.png" alt="">
+                    <p>2018 / 5 / 19</p>
+                    <p>あのネオンはいつ交換するのか！？！？観覧車の謎に迫る！</p>
+                    <div class='readmore1'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_3.png" alt="">
+                    <p>2018 / 5 / 18</p>
+                    <p>ラソナの社内はこんなのよ</p>
+                    <div class='readmore'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_4.png" alt="">
+                    <p>2018 / 5 / 27</p>
+                    <p>お隣のアラハはハワイ？</p>
+                    <div class='readmore'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_5.png" alt="">
+                    <p>2018 / 5 / 16</p>
+                    <p>なぜテント？ラソナの人に聞いてみた</p>
+                    <div class='readmore'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+                <div class='article-wrap'>
+                    <img src="./img/post_img_6.png" alt="">
+                    <p>2018 / 5 / 15</p>
+                    <p>ベイエリアおしゃれすぎる問題</p>
+                    <div class='readmore'>
+                        <a href="">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+          </section>    
+        </div>      
+      </div>    
+    </main>
+    <footer>
+        Copyright <span>&copy</span>Shinohara.ALL RIGHTS RESERVED.</footer>
+</body>
+</html>
